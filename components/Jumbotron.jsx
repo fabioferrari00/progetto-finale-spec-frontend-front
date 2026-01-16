e728dcad554d821a63d9f740b21fb8d1d0ff9915
fabioferrari00 <fabioferarri.ff00@gmail.com>
2026-01-16 14:35:27 +0100

    jumbotron

--- components/Jumbotron.jsx
+++ components/Jumbotron.jsx
@@ -3,11 +3,16 @@ import React from "react";
 const Jumbotron = () => {
   return (
     <>
-      <div className="bg-lg-blue">
+      <div className="jumbo-bg">
         <div className="container">
-          <div className="row">
-            <div className="col-6">ciao</div>
-            <div className="col-6">ciao</div>
+          <div className="row d-flex align-items-center">
+            <div className="col-6">
+              <img className="jumbo-img" src="../src/assets/logo.png" alt="" />
+            </div>
+            <div className="col-6 bg-jumbo-title">
+              <h2 className="jumbo-title">Cerca il corso giusto per te.</h2>
+              <h2 className="jumbo-title2">Confrontali tra loro e valuta quale fa al caso tuo!</h2> 
+            </div>
           </div>
         </div>
       </div>
